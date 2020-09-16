9be106e3fd31ae7165eed632b370351d2de9f1a5
Abner Joia <abnerjoia1991@gmail.com>
2020-09-15 23:33:11 -0300

    Entities, Repositories

--- backend/src/main/java/com/devsuperior/dspesquisa/DspesquisaApplication.java
+++ backend/src/main/java/com/devsuperior/dspesquisa/DspesquisaApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class dspesquisaApplication {
+public class DspesquisaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(dspesquisaApplication.class, args);
+		SpringApplication.run(DspesquisaApplication.class, args);
 	}
 
 }
